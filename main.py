344a5d87f3a3b4f8f1393ed8a24213dab33fffd3
crpier <crpier42@gmail.com>
2023-02-19 00:23:04 +0200

    bugfixes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Callable, NewType
 from pytest import fixture, FixtureRequest
 from _pytest.fixtures import FixtureFunctionMarker, FixtureDef
+from inspect import signature
 
 new_fixtures: list[Callable] = []
 
@@ -11,44 +12,39 @@ def fixture_generator(request: FixtureRequest):
     fixturedef = FixtureDef(
         fixturemanager=request._fixturemanager,
         baseid="",
-        argname="User",
-        func=User,
+        argname="UserId",
+        func=UserId,
         scope="function",
         params=None,
         unittest=False,
         ids=None,
     )
-    request._fixturemanager._arg2fixturedefs["User"] = [fixturedef]
+    request._fixturemanager._arg2fixturedefs["UserId"] = [fixturedef]
 
 
 def new_fixture(wrapped_func: Callable):
     marker = FixtureFunctionMarker(
         scope="function", params=None, name=wrapped_func.__name__
     )
-    new_fixture = marker(wrapped_func)
-    wrapped_func = new_fixture
+    wrapped_sig = signature(wrapped_func)
+    fixtured_func = marker(wrapped_func)
     new_fixtures.append(wrapped_func)
 
-    new_class = type(wrapped_func.__name__, (), {"__call__": wrapped_func})
-    return new_class
+    new_class = type(fixtured_func.__name__, (), {"__call__": fixtured_func})
+    return fixtured_func
 
 
 def new_test(wrapped_func: Callable):
-    def dynamically_generated_func(User: int):
-        return wrapped_func(User)
+    def dynamically_generated_func(UserId: int):
+        return wrapped_func(UserId)
 
     return dynamically_generated_func
 
 
 @new_fixture
-def User() -> int:
+def UserId() -> int:
     return 32
 
-
-def pula(kek: int):
-    print(kek)
-
-
 @new_test
-def test_user(user: User):
-    assert True
+def test_user(user: UserId):
+    assert user == 32
