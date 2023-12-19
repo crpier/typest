b6109ec57bf9b0bfa8dd5545fd3932df4eac4f74
crpier42 <crpier42@gmail.com>
2023-12-19 03:36:05 +0200

    Rewrites and improvements
    - change the way this works: instead of messing around with pytest
      the decorator for tests simply renames the args of the functions
    - add a new way to define fixtures in tests, using Annotated

--- main.py
+++ main.py
@@ -1,39 +1,46 @@
-from functools import partial
-from typing import Callable, ParamSpec, Type, TypeVar
 import pytest
-from _pytest.fixtures import FixtureDef
+import inspect
+from typing import Any, Callable, ParamSpec, Type, TypeVar, get_args
 from inspect import signature
-
-new_fixtures: list[Callable] = []
 
 P = ParamSpec("P")
 T = TypeVar("T")
 
 
-@pytest.fixture(autouse=True)
-def fixture_generator(request: pytest.FixtureRequest):
-    for new_fixture in new_fixtures:
-        request.fixturenames.append(new_fixture.__name__)
-        fixturedef = FixtureDef(
-            fixturemanager=request._fixturemanager,
-            baseid="",
-            argname=new_fixture.__name__,
-            func=new_fixture,
-            scope="function",
-            params=None,
-            unittest=False,
-            ids=None,
-        )
-        request._fixturemanager._arg2fixturedefs[fixture.__name__] = [fixturedef]
+def cringe_fixture(*args, **kwargs) -> Any:
+    return pytest.fixture(*args, **kwargs)
 
 
-def fixture(func: Callable[P, T]) -> Type[T]:
-    new_fixtures.append(func)
-    return func  # type: ignore
-
-
-def some_test(func: Callable[..., None]):
+def cringe_check(func: Callable[..., None]):
+    renames = {}
     sig = signature(func)
-    params = [value.annotation() for value in sig.parameters.values()]
-    new_func = partial(func, *params)
-    return new_func
+    for name, val in sig.parameters.items():
+        renames[name] = val.annotation.__name__
+    return _rename_parameters(func, renames)
+
+
+def check(func: Callable[..., None]):
+    renames = {}
+    sig = signature(func)
+    for name, val in sig.parameters.items():
+        annotation = get_args(val.annotation)
+        fixture_name = annotation[1].__name__
+        renames[name] = fixture_name
+    return _rename_parameters(func, renames)
+
+
+def _rename_parameters(func, rename_dict):
+    original_sig = inspect.signature(func)
+    new_params = [
+        param.replace(name=rename_dict.get(param.name, param.name))
+        for param in original_sig.parameters.values()
+    ]
+    new_sig = original_sig.replace(parameters=new_params)
+
+    def wrapper(*args, **kwargs):
+        bound_args = new_sig.bind(*args, **kwargs)
+        return func(*bound_args.args, **bound_args.kwargs)
+
+    wrapper.__signature__ = new_sig
+
+    return wrapper
--- test.py
+++ test.py
@@ -1,11 +1,34 @@
-from main import fixture, some_test
+from typing import Annotated
+
+import pytest
+from main import cringe_fixture, cringe_check, check
+
+A = Annotated
+
+# This seems like a concise way to go about it, but it's weird because
+# - the fixture is named like class to make the annotation look not surprising
+#   - the reverse would be to make the annotation look surprising, in order to make the fixture look normal
+# - the "user" param's type is misleading: it's not `UserID`, it's `int`
 
 
-@fixture
-def UserId() -> int:
+@cringe_fixture
+def UserID() -> int:
     return 32
 
 
-@some_test
-def test_user(user: UserId):
+@cringe_check
+def test_user(user: UserID):
     assert user == 32
+
+
+# That seems to me like a nicer way to go about it, but it's more verbose
+
+
+@pytest.fixture
+def new_sku() -> str:
+    return "42"
+
+
+@check
+def test_sku(sku: A[str, new_sku]):
+    assert sku == "42"
